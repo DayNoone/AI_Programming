1605bc8a5e42c246f20b2ec8dce1eda28cc96198
Markus Lund <markuslund92@hotmail.com>
2015-09-08 14:00:49 +0200

    Started on main method

--- Project_1/Module_1/main.py
+++ Project_1/Module_1/main.py
@@ -1,1 +1,45 @@
-__author__ = 'DagInge'
+# coding=utf-8
+import math
+from heapq import heappush, heappop
+
+MOVEMENT_COST = 1
+
+
+def calculateHValue(initNode, goalNode):
+    xOff = math.fabs(goalNode.X - initNode.X)
+    yOff = math.fabs(goalNode.Y - initNode.Y)
+    return xOff + yOff
+
+
+def calculateGValue(initNode):
+    return initNode.parent.g + MOVEMENT_COST
+
+
+def generate_all_successors(x):
+    pass
+
+
+def best_first_search(initNode, goalNode, ):
+    closedNodes = []
+    openNodes = []
+    initNode.g = calculateGValue(initNode)
+    initNode.h = calculateHValue(initNode, goalNode)
+
+    heappush(openNodes, initNode)
+
+    while (True):
+        if openNodes.count() == 0:
+            print "No solution found"
+            break
+        x = heappop(open)
+        heappush(closedNodes, x)
+        if x.X == goalNode.X and x.X == goalNode.Y:
+            print "Solution found!"
+            break
+
+        succ = generate_all_successors(x)
+
+        for s in succ:
+            # If node S* has previously been created, and if state(S*) = state(S), then S ← S*.
+            x.kids.append(s)
+
