280daf3fae694b54edcc10ee202623e5051b1971
DagInge <dihelgoy@gmail.com>
2015-09-10 13:16:36 +0200

    Coloring

--- Project_1/Module_1/main.py
+++ Project_1/Module_1/main.py
@@ -124,7 +124,7 @@ def best_first_search(initNode, goalPos, board):
                     propagate_path_improvements(s)
 
 
-MOVEMENT_COST = 0.5
+MOVEMENT_COST = 1
 
 board_test = Board(generateBoard(6, 6, 1, 0, 5, 5, [[3, 2, 2, 2], [0, 3, 1, 3], [2, 0, 4, 2], [2, 5, 2, 1]]),
                    (1, 0), (5, 5))
