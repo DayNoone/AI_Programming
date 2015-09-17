f5335204a586b14f88a31adb62a6532fd24d63bc
Dag Inge Helgøy <dihelgoy@gmail.com>
2015-09-17 14:10:26 +0200

    Delete Node.py

--- Project_1/Module_2/Node.py
+++
@@ -1,7 +0,0 @@
-from Project_1.genericAStarNode import AStarNode
-
-class Node(AStarNode):
-	def __init__(self, variables, initialDomain):
-		self.variables = variables
-		self.domainSize = initialDomain
-		print initialDomain
