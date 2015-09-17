8b895d0be6794efccefad8360d8b66b76abd9f00
Dag Inge Helgøy <dihelgoy@gmail.com>
2015-09-17 14:10:20 +0200

    Delete Variable.py

--- Project_1/Module_2/Variable.py
+++
@@ -1,8 +0,0 @@
-class Variable:
-    def __init__(self, id, x, y, initDomain):
-        self.id = id
-        self.xPos = x
-        self.yPos = y
-        self.neighbor = []
-        self.colorid = None
-        self.domain = initDomain
